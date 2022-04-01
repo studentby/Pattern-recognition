1b2a7767236139d913d1fc78e76ba34ab758636d
Ayhan-perf <Ayhan_Gylychmammedov@epam.com>
2022-04-01 18:29:31 +0300

    added automatiacla finder for polygones

--- PiVS_PiSi.py
+++ PiVS_PiSi.py
@@ -1,107 +1,94 @@
+from email.mime import image
 import matplotlib.pyplot as plt
-
+import time
 from tkinter import Image
 from PIL import Image
 from pyparsing import line_end
 from skimage.draw import line
+import cv2
+import math
 
 from skimage.color import rgb2gray
 from skimage.filters import gaussian
 from skimage.segmentation import active_contour
-from skimage.measure import find_contours, approximate_polygon
-from skimage import color, morphology
+from skimage.measure import find_contours, approximate_polygon, perimeter
+from skimage.future import graph
+from skimage import color, morphology, feature, segmentation, filters, io
 import numpy as np
 
-original_image = Image.open("Pi/Pi50.jpg")
-original_image_1 = Image.open("PiSi/PiSi50.jpg")
 
-# im.convert('L')
+def main():
+    original_image = Image.open("Pi/Pi50.jpg")
+    original_image_1 = Image.open("PiSi/PiSi50.jpg")
 
-im_array = np.array(original_image)
-im1_array = np.array(original_image_1)
+    im_array = np.array(original_image)
+    im1_array = np.array(original_image_1)
 
-# Image saved in numPy array
-npImage = Image.fromarray(im_array, 'RGB')
-npImage1 = Image.fromarray(im1_array, 'RGB')
+    # Image saved in numPy array
+    npImage = Image.fromarray(im_array, 'RGB')
+    npImage1 = Image.fromarray(im1_array, 'RGB')
 
-# Grayscaling and comparison of original image in MatPlot Lib 
-grayscale_image = rgb2gray(original_image)
-grayscale_image_1 = rgb2gray(original_image_1)
+    # Grayscaling and comparison of original image in MatPlot Lib 
+    grayscale_image = rgb2gray(original_image)
+    grayscale_image_1 = rgb2gray(original_image_1)
 
-s = np.linspace(0, 2 * np.pi, 400)
-r =  568 + 200*np.cos(s) # x -coordinate
-c = 841 + 200*np.sin(s) # y - coordinate
-init = np.array([r, c]).T
+    # Showing made changes on Plot and comparing with Original 
+    fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+    ax = axes.ravel()
 
+    # Show conturus on picture
+    footprint = morphology.disk(10)
+    res = morphology.white_tophat(grayscale_image,footprint)
 
+    contours = find_contours(grayscale_image - res, fully_connected='high')
+    for contour in contours:
+        ax[1].plot(contour[:, 1], contour[:, 0], linewidth = 1)  
 
-# snake = active_contour(gaussian(grayscale_image, 3, preserve_range=False),
-#                        init, alpha=0.015, beta=10, gamma=0.001)
+    footprint_1 = morphology.disk(5)
+    res_1 = morphology.white_tophat(grayscale_image_1, footprint_1)
 
+    # Detecting Edges 
+    edges = feature.canny(grayscale_image - res, sigma = 1)
+    edges1 = feature.canny(grayscale_image_1 - res_1, sigma = 2)
 
-# Showing made changes on Plot and comparing with Original 
-fig, axes = plt.subplots(1, 2, figsize=(8, 4))
-ax = axes.ravel()
-
-# Show conturus on picture
-footprint = morphology.disk(5)
-res = morphology.white_tophat(grayscale_image,footprint)
-
-contours = find_contours(grayscale_image - res, fully_connected='high')
-for contour in contours:
-    ax[1].plot(contour[:, 1], contour[:, 0], linewidth = 1)  
-
-
-footprint_1 = morphology.disk(5)
-res_1 = morphology.white_tophat(grayscale_image_1,footprint_1)
+    # Image rendition
+    ax[1].imshow(edges, cmap=plt.cm.gray)
+    ax[0].imshow(grayscale_image_1 - res_1, cmap=plt.cm.gray)
 
 
-ax[1].imshow(grayscale_image - res, cmap=plt.cm.gray)
-ax[0].imshow(grayscale_image_1 - res_1, cmap=plt.cm.gray)
-# ax[1].plot(init[:, 1], init[:, 0], '--r', lw=3)
-# ax[1].plot(snake[:, 1], snake[:, 0], '-b', lw=3)
+    ax[0].set_xticks([]), ax[0].set_yticks([])
+    ax[1].set_xticks([]), ax[1].set_yticks([])
 
-# Removal of axis label coordinates 
-ax[1].set_xticks([]), ax[1].set_yticks([])
-ax[0].set_xticks([]), ax[0].set_yticks([])
+    ax[1].axis([0, grayscale_image.shape[1], grayscale_image.shape[0], 0])
 
-ax[1].axis([0, grayscale_image.shape[1], grayscale_image.shape[0], 0])
+    ax[1].set_title("№14 50 gramm pressure", fontsize = 20)
+    ax[0].set_title("PolySi", fontsize = 20)
 
-ax[1].set_title("Polyimide 50 gramm pressure")
-ax[0].set_title("Polyimide/Si 50 gramm pressure")
+    contours_1 = find_contours(grayscale_image_1 - res_1, fully_connected='high')
 
-# Drawing square
-coordinates = np.array(
-    [line(831, 761, 660, 581),
-    line(660, 581, 837, 396),
-    line(837, 396, 1000, 562),
-    line(1000, 562, 831, 761)],
-    dtype=list)
+    for contour in contours_1:
+        coords = approximate_polygon(contour, tolerance = 60)
+        ax[0].plot(coords[:, 1], coords[:, 0], '-r', linewidth = 1)
+        ax[0].plot(contour[:, 1], contour[:, 0], linewidth = 2)
+        if len(coords) == 4:
+            print(coords)
+            # coords[x][y]
+            print(math.sqrt(math.pow(coords[0][0]-coords[2][0],2) + math.pow(coords[0][1]-coords[2][1], 2)))
+            print(math.sqrt(math.pow(coords[1][0]-coords[3][0],2) + math.pow(coords[1][1]-coords[3][1], 2)))
+            
+            c = np.expand_dims(coords.astype(np.float32), 1)
+            # Convert it to UMat object
+            c = cv2.UMat(c)
+            area = cv2.contourArea(c)
+            # print(area)
+        
+        
+    fig.tight_layout()
 
-for i in range(len(coordinates)):
-    rr, cc = coordinates[i]
-    line_draw = np.array([rr,cc]).T 
-    ax[1].plot(line_draw[:, 0],line_draw[:, 1],'--b',lw=1)
-
-coordinates_1 = np.array(
-    [line(3101, 1857, 2845, 1573),
-    line(2845, 1573, 3105, 1304),
-    line(3101, 1304, 3336, 1573),
-    line(3336, 1573, 3101, 1857)],
-    dtype=list)
-
-
-
-for i in range(len(coordinates_1)):
-    rr_1, cc_1 = coordinates_1[i]
-    line_draw_1 = np.array([rr_1,cc_1]).T 
-    ax[0].plot(line_draw_1[:, 0],line_draw_1[:, 1],'--w',lw=1)
-
-contours_1 = find_contours(grayscale_image_1 - res_1, fully_connected='high')
-for contour in contours_1:
-    ax[0].plot(contour[:, 1], contour[:, 0], linewidth = 3) 
-
-fig.tight_layout()
-plt.show()
-
+if __name__=="__main__":
+    start_time = time.time()
+    main()
+    print("---Execution %s seconds ---" % round(time.time() - start_time))
+    plt.show()
+    
 # npImage.save('/home/ububntu/Desktop/Diploma/squrePatterns/image_test.png')
--- PoSiVS14_50gramm.py
+++ PoSiVS14_50gramm.py
@@ -6,6 +6,7 @@ from PIL import Image
 from pyparsing import line_end
 from skimage.draw import line
 import cv2
+import math
 
 from skimage.color import rgb2gray
 from skimage.filters import gaussian
@@ -46,16 +47,16 @@ def main():
     for contour in contours:
         ax[1].plot(contour[:, 1], contour[:, 0], linewidth = 1)  
 
-    footprint_1 = morphology.disk(10)
+    footprint_1 = morphology.disk(5)
     res_1 = morphology.white_tophat(grayscale_image_1, footprint_1)
 
     # Detecting Edges 
-    # edges = feature.canny(grayscale_image, sigma = 5)
-    # edges1 = feature.canny(grayscale_image_1,sigma = 5)
+    edges = feature.canny(grayscale_image - res, sigma = 1)
+    edges1 = feature.canny(grayscale_image_1 - res_1, sigma = 2)
 
     # Image rendition
-    ax[1].imshow(grayscale_image, cmap=plt.cm.gray)
-    ax[0].imshow(grayscale_image_1, cmap=plt.cm.gray)
+    ax[1].imshow(edges, cmap=plt.cm.gray)
+    ax[0].imshow(grayscale_image_1 - res_1, cmap=plt.cm.gray)
 
 
     ax[0].set_xticks([]), ax[0].set_yticks([])
@@ -66,48 +67,23 @@ def main():
     ax[1].set_title("№14 50 gramm pressure", fontsize = 20)
     ax[0].set_title("PolySi", fontsize = 20)
 
-    # Drawing square with coordinates (x1,y1,x2,y2)
-
-    coordinates = np.array(
-        [line(2208, 1688, 2085, 1564),
-        line(2085, 1564, 2207, 1453),
-        line(2207, 1453, 2336, 1564),
-        line(2336, 1564, 2208, 1688)],
-        dtype=list)
-
-    for i in range(len(coordinates)):
-        rr, cc = coordinates[i]
-        line_draw = np.array([rr,cc]).T 
-        ax[1].plot(line_draw[:, 0],line_draw[:, 1],'--b',lw=1)
-
-    coordinates_1 = np.array(
-        [line(1769, 1191, 1727, 1154),
-        line(1727, 1154, 1769, 1116),
-        line(1769, 1116, 1812, 1154),
-        line(1812, 1154, 1769, 1191)],
-        dtype=list)
-
-
-
-    for i in range(len(coordinates_1)):
-        rr_1, cc_1 = coordinates_1[i]
-        line_draw_1 = np.array([rr_1,cc_1]).T 
-        ax[0].plot(line_draw_1[:, 0],line_draw_1[:, 1],'--w', lw = 1)
-
     contours_1 = find_contours(grayscale_image_1 - res_1, fully_connected='high')
-    
+
     for contour in contours_1:
-        coords = approximate_polygon(contour, tolerance = 40)
+        coords = approximate_polygon(contour, tolerance = 60)
         ax[0].plot(coords[:, 1], coords[:, 0], '-r', linewidth = 1)
         ax[0].plot(contour[:, 1], contour[:, 0], linewidth = 2)
         if len(coords) == 4:
-            print(len(coords))
             print(coords)
+            # coords[x][y]
+            print(math.sqrt(math.pow(coords[0][0]-coords[2][0],2) + math.pow(coords[0][1]-coords[2][1], 2)))
+            print(math.sqrt(math.pow(coords[1][0]-coords[3][0],2) + math.pow(coords[1][1]-coords[3][1], 2)))
+            
             c = np.expand_dims(coords.astype(np.float32), 1)
             # Convert it to UMat object
             c = cv2.UMat(c)
             area = cv2.contourArea(c)
-            print(area)
+            # print(area)
         
         
     fig.tight_layout()
@@ -116,6 +92,6 @@ if __name__=="__main__":
     start_time = time.time()
     main()
     print("---Execution %s seconds ---" % round(time.time() - start_time))
-    # plt.show()
+    plt.show()
     
 # npImage.save('/home/ububntu/Desktop/Diploma/squrePatterns/image_test.png')
